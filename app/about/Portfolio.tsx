623b3b03a199681a7b6f6677fc338c4cae47dec5
datharnu <olagbemisoyee@gmail.com>
2024-01-21 11:24:20 -0800

    mdmdmd

--- app/about/Portfolio.tsx
+++ app/about/Portfolio.tsx
@@ -21,15 +21,11 @@ const PortfolioItem: React.FC<PortfolioItemProps> = ({
 
   return (
     <div
-      className="relative border-secondary sm:border-black lg:border-4 sm:border-2  overflow-hidden"
+      className="relative   shadow-secondary  shadow-md mx-5 sm:flex rounded-xl  overflow-hidden"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Image
-        src={imageSrc}
-        alt={alt}
-        className=" h-[25rem] sm:h-[5rem] object-contain"
-      />
+      <Image src={imageSrc} alt={alt} className=" h-[25rem] object-contain" />
       {isHovered && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <p className={` font-bold text-lg ${textColor}`}>{text}</p>
@@ -45,31 +41,37 @@ export default function Portfolio() {
       <h1 className="font-bold text-[40px] sm:text-center lg:my-10 lg:ml-20 sm:text-[20px] ">
         Portfolio
       </h1>
-      <div className="flex lg:mx-20 px-2 sm:my-4 sm:gap-2 gap-5">
-        <a href="https://chowdeck-alpha.vercel.app/">
-          <PortfolioItem
-            imageSrc={design3}
-            alt="Responsive Design"
-            text="Project 1"
-            textColor="text-blue-500" // Customize text color here
-          />
-        </a>
-        <a href="https://news-homepage-gules.vercel.app/">
-          <PortfolioItem
-            imageSrc={design2}
-            alt="Design"
-            text="Project 2"
-            textColor="text-green-500" // Customize text color here
-          />
-        </a>
-        <a href="https://notification-page-on1p.vercel.app/">
-          <PortfolioItem
-            imageSrc={design1}
-            alt="Design"
-            text="Project 3"
-            textColor="text-red-500" // Customize text color here
-          />
-        </a>
+      <div className="lg:flex lg:mx-20 px-2 sm:my-4 sm:space-y-10 gap-5  ">
+        <div>
+          <a href="https://chowdeck-alpha.vercel.app/">
+            <PortfolioItem
+              imageSrc={design3}
+              alt="Responsive Design"
+              text="Project 1"
+              textColor="text-blue-500" // Customize text color here
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://news-homepage-gules.vercel.app/">
+            <PortfolioItem
+              imageSrc={design2}
+              alt="Design"
+              text="Project 2"
+              textColor="text-green-500" // Customize text color here
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://notification-page-on1p.vercel.app/">
+            <PortfolioItem
+              imageSrc={design1}
+              alt="Design"
+              text="Project 3"
+              textColor="text-red-500" // Customize text color here
+            />
+          </a>
+        </div>
       </div>
     </section>
   );
